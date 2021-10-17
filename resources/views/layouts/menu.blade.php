ae60ef93cc0d2c6b5cbd2af10d670528698d16f5
failamir <38488480+failamir@users.noreply.github.com>
2021-10-17 23:13:10 +0700

    q

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,19 +3,4 @@
         <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
 </li>
-<li class="side-menus {{ Request::is('safitris*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('safitris.index') }}">
-        <i class="fas fa-edit"></i><span>Kategori</span>
-    </a>
-</li>
-<li class="side-menus {{ Request::is('safitris*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('safitris.index') }}">
-        <i class="fas fa-edit"></i><span>Buku</span>
-    </a>
-</li>
-<li class="side-menus {{ Request::is('safitris*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('safitris.index') }}">
-        <i class="fas fa-edit"></i><span>Peminjaman</span>
-    </a>
-</li>
 
